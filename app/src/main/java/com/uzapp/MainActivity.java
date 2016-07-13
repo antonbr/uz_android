a4b429143fed8e99c8d5498a27ec07e1d5e970e7
Viktoria Tsyrulnyk <viktoria.tsyrulnyk@gmail.com>
2016-07-13 22:06:33 +0300

    Create view for selecting from and to station

--- app/src/main/java/com/uzapp/MainActivity.java
+++ app/src/main/java/com/uzapp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.uzapp;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.uzapp.view.search.SearchFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        replaceFragment(new SearchFragment(), false, null);
+    }
+
+
+    public void replaceFragment(Fragment f, boolean addToBackStack, String tag) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        if (addToBackStack) {
+            transaction.addToBackStack(tag);
+        }
+        transaction.replace(R.id.fragmentContainer, f, tag);
+        transaction.commit();
     }
 }
---
+++ app/src/main/java/com/uzapp/view/search/SearchFragment.java
@@ -0,0 +1,48 @@
+package com.uzapp.view.search;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import com.uzapp.R;
+
+import butterknife.BindDimen;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+
+/**
+ * Created by vika on 13.07.16.
+ */
+public class SearchFragment extends Fragment {
+    @BindView(R.id.pathFrom) EditText pathFrom;
+    @BindView(R.id.pathTo) EditText pathTo;
+    @BindDimen(R.dimen.hint_padding) int hintPadding;
+    private Unbinder unbinder;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.search_fragment, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @OnClick(R.id.useLocationBtn)
+    void onLocationBtnClicked(CheckableImageView imageButton) {
+        imageButton.toggle();
+        pathFrom.setTranslationY(imageButton.isChecked() ? hintPadding : 0);
+       // pathFrom.setText(imageButton.isChecked() ? "Ивано-Франковск" : "");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+}
