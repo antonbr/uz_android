91b9da10ea30848758f1a92b8b9b7df2e7a8bdb6
Viktoria Tsyrulnyk <viktoria.tsyrulnyk@gmail.com>
2016-08-10 22:35:03 +0300

    Finish ui for create account page

---
+++ app/src/main/java/com/uzapp/view/login/LoginFlowActivity.java
@@ -0,0 +1,18 @@
+package com.uzapp.view.login;
+
+import android.os.Bundle;
+
+import com.uzapp.R;
+import com.uzapp.view.BaseActivity;
+
+/**
+ * Created by vika on 09.08.16.
+ */
+public class LoginFlowActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        replaceFragment(new CreateAccountFragment(), false);
+    }
+}
