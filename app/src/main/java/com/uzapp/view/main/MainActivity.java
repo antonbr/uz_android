aeff529c24b73e6df193900869c580362bd0dfee
Vladimir <vmazur27@gmail.com>
2016-08-13 04:39:41 +0300

    implemented bottom navigation bar and created ui menu

--- app/src/main/java/com/uzapp/view/main/MainActivity.java
+++ app/src/main/java/com/uzapp/view/main/MainActivity.java
@@ -1,17 +1,62 @@
 package com.uzapp.view.main;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
 import com.uzapp.R;
 import com.uzapp.view.BaseActivity;
+import com.uzapp.view.main.menu.MenuFragment;
 import com.uzapp.view.main.search.SearchFragment;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements AHBottomNavigation.OnTabSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createNavItems();
+    }
+
+    private void createNavItems() {
+        AHBottomNavigation bottomNavigationBar = (AHBottomNavigation) findViewById(R.id.bottomNavigationBar);
+        if (bottomNavigationBar != null) {
+            bottomNavigationBar.setOnTabSelectedListener(this);
+        }
+        //CREATE ITEMS
+        AHBottomNavigationItem searchTrainItem = new AHBottomNavigationItem(null, R.drawable.ic_search_train);
+        AHBottomNavigationItem myTicketsItem = new AHBottomNavigationItem(null, R.drawable.ic_my_tickets);
+        AHBottomNavigationItem bottomMenuItem = new AHBottomNavigationItem(null, R.drawable.ic_bottom_menu);
+        //ADD THEM to bar
+        bottomNavigationBar.addItem(searchTrainItem);
+        bottomNavigationBar.addItem(myTicketsItem);
+        bottomNavigationBar.addItem(bottomMenuItem);
+        //set properties
+        bottomNavigationBar.setDefaultBackgroundColor(getResources().getColor(android.R.color.white));
+        bottomNavigationBar.setAccentColor(getResources().getColor(R.color.bgButtonWagonPlacesSelected));
+        //set current item
+        bottomNavigationBar.setCurrentItem(0);
         replaceFragment(new SearchFragment(), false);
+    }
+
+    @Override
+    public void onTabSelected(int position, boolean wasSelected) {
+        switch (position) {
+            case 0:
+                if (!wasSelected)
+                    replaceFragment(new SearchFragment(), false);
+                break;
+            case 1:
+                Toast.makeText(MainActivity.this, "My Tickets", Toast.LENGTH_SHORT).show();
+                break;
+            case 2:
+                if (!wasSelected)
+                    addFragment(new MenuFragment(), R.anim.slide_up, R.anim.slide_down);
+                break;
+            default:
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/uzapp/view/main/menu/MenuFragment.java
@@ -0,0 +1,55 @@
+package com.uzapp.view.main.menu;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.uzapp.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MenuFragment extends Fragment {
+
+    @BindView(R.id.btnLoginYourAccount)
+    Button btnLoginYourAccount;
+
+    private Unbinder unbinder;
+
+    public MenuFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_menu, container, false);
+
+        unbinder = ButterKnife.bind(this, view);
+
+        return view;
+    }
+
+    @OnClick(R.id.btnLoginYourAccount)
+    void onClickBtnLoginYourAccount() {
+        Toast.makeText(getActivity(), "Login your account", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+}
