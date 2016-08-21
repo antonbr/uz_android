b7bcc23faf2482e96eaec1e530e08c455ba8a873
Viktoria Tsyrulnyk <viktoria.tsyrulnyk@gmail.com>
2016-08-21 13:11:03 +0300

    Add loader in profile page

--- app/src/main/java/com/uzapp/view/main/profile/ProfileFragment.java
+++ app/src/main/java/com/uzapp/view/main/profile/ProfileFragment.java
@@ -10,6 +10,8 @@ import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.uzapp.R;
@@ -44,6 +46,8 @@ public class ProfileFragment extends Fragment {
     @BindView(R.id.phoneNumber) TextView phoneNumber;
     @BindView(R.id.studentId) TextView studentId;
     @BindView(R.id.myTicketsCount) TextView myTicketsCount;
+    @BindView(R.id.progressBar) ProgressBar progressBar;
+    @BindView(R.id.mainScrollView) ScrollView mainScrollView;
     private Unbinder unbinder;
     private User user;
 
@@ -52,6 +56,8 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.profile_fragment, container, false);
         unbinder = ButterKnife.bind(this, view);
+        progressBar.setVisibility(View.VISIBLE);
+        mainScrollView.setVisibility(View.GONE);
         Call call = ApiManager.getApi(getContext()).getUser();
         call.enqueue(userCallback);
         return view;
@@ -100,6 +106,7 @@ public class ProfileFragment extends Fragment {
 
     private void showUserInfo() {
         if (user != null) {
+            mainScrollView.setVisibility(View.VISIBLE);
             bonusCardNumber.setText("");
             firstName.setText(user.getFirstName());
             lastName.setText(user.getLastName());
@@ -144,6 +151,7 @@ public class ProfileFragment extends Fragment {
             if (requestCode == REQUEST_EDIT_PROFILE) {
                 user = Parcels.unwrap(data.getParcelableExtra("user"));
                 showUserInfo();
+                Snackbar.make(getView(), R.string.profile_edit_successful_result, Snackbar.LENGTH_SHORT).show();
             }
         }
     }
@@ -153,6 +161,7 @@ public class ProfileFragment extends Fragment {
         @Override
         public void onResponse(Call<User> call, Response<User> response) {
             if (getView() != null) {
+                progressBar.setVisibility(View.GONE);
                 if (response.isSuccessful()) {
                     user = response.body();
                     showUserInfo();
@@ -165,6 +174,7 @@ public class ProfileFragment extends Fragment {
         @Override
         public void onFailure(Call<User> call, Throwable t) {
             if (getView() != null) {
+                progressBar.setVisibility(View.GONE);
                 Snackbar.make(getView(), t.getMessage(), Snackbar.LENGTH_SHORT).show();
             }
         }
