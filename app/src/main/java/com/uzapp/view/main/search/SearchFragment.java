30b8b5bd82cee761c13f12360e6f9178cc5f4856
Viktoria Tsyrulnyk <viktoria.tsyrulnyk@gmail.com>
2016-09-08 14:42:09 +0300

    Remove test code

--- app/src/main/java/com/uzapp/view/main/search/SearchFragment.java
+++ app/src/main/java/com/uzapp/view/main/search/SearchFragment.java
@@ -28,7 +28,6 @@ import com.uzapp.R;
 import com.uzapp.network.ApiManager;
 import com.uzapp.pojo.RouteHistoryItem;
 import com.uzapp.pojo.Station;
-import com.uzapp.pojo.User;
 import com.uzapp.util.ApiErrorUtil;
 import com.uzapp.util.CommonUtils;
 import com.uzapp.util.Constants;
@@ -57,7 +56,6 @@ import butterknife.OnCheckedChanged;
 import butterknife.OnClick;
 import butterknife.OnTextChanged;
 import butterknife.Unbinder;
-import io.realm.Realm;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -111,9 +109,6 @@ public class SearchFragment extends Fragment implements GoogleApiClient.Connecti
         initArguments();
         initDatePickerList();
         checkAllFieldsFilled();
-        Realm realm = Realm.getDefaultInstance();
-        User user = realm.where(User.class).findFirst();
-        realm.close();
         return view;
     }
 
